4e90f4fbfcd459fad60e8f5e9771759a54ceeab8
maryamch486 <maryamsadiq1616@gmail.com>
2020-12-05 08:45:04 -0300

    simple

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'Simple.dart';
+import 'Hard.dart';
+
 void main(){
   runApp(new MaterialApp(
     home: new MyApp(),
@@ -49,6 +52,61 @@ class AfterSplash extends StatelessWidget {
           ),),
 
       ),
+    );
+  }
+}
+class SimpleHard extends StatefulWidget {
+  @override
+  _SimpleHardState createState() => _SimpleHardState();
+}
+
+class _SimpleHardState extends State<SimpleHard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Home',
+        home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        RaisedButton(
+        onPressed: () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => Simple()));
+    },
+    child: Text(
+    'Simple',
+    style: TextStyle(
+    color: Colors.white,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    ),
+    ),
+    ),
+    SizedBox(
+    height: 40.0,
+    width: 400.0,
+    ),
+    RaisedButton(
+    onPressed: () {
+    Navigator.pushReplacement(
+    context, MaterialPageRoute(builder: (context) => Hard()));
+    },
+    child: Text(
+    'Hard',
+    style: TextStyle(
+    color: Colors.white,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    ),
+    ),
+    ),
+      ],
+    ),
+    ),
+    ),
     );
   }
 }
--- lib/navigation_drawer.dart
+++ lib/navigation_drawer.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
                       child: Text(
                         'Hard',
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.yellow,
                           fontSize: 20.0,
                         ),
                       ),
